b6d193caa9666a46e1b4190ba1a50d2e970bc757
McFly91 <martin91200@gmail.com>
2020-12-06 21:48:23 +0100

    Création du create, getOne et getAll Post API

--- Backend/controllers/post.js
+++ Backend/controllers/post.js
@@ -7,9 +7,27 @@ const inputRegex = /^[^\s@&"()!_$*€£`+=\/;?#<>]*[A-Za-z0-9]{1,}/;
 
 exports.createPost = (req, res, next) => {
     const postObject = JSON.parse(req.body.post);
-
     const filename = req.file.filename;
-
+    if (filename) {
+            postObject.contenu_media = `${req.protocol}://${req.get("host")}/images/${filename}`;
+    }
+    console.log(postObject)
+    if (inputTitreRegex.test(postObject.titre) && (postObject.contenu_text === null || (postObject.contenu_text !== null && inputRegex.test(postObject.contenu_text)))) {
+        postModel.postSchema(
+            postObject.titre, 
+            postObject.contenu_text, 
+            postObject.contenu_media, 
+            res.locals.userId);
+        res.status(201).json({ message : "Post enregistrée" });
+    }
+    else {
+        if (filename !== null) {
+            fs.unlink(`images/${filename}`, () => {
+                res.status(400);
+            })
+        }
+        return res.status(400).json({ error : "Erreur dans l'entrée des données, veuillez rentrer des informations pertinantes" });
+    }
 };
 
 exports.modifyPost = (req, res, next) => {
@@ -21,11 +39,15 @@ exports.deletePost = (req, res, next) => {
 };
 
 exports.getOnePost = (req, res, next) => {
-    postModel.onePost(req.params.id);  
+    postModel.onePost(req.params.id)
+        .then(response => res.status(200).json(response))
+        .catch(error => res.status(500).json({ error }))
 };
 
 exports.getAllPosts = (req, res, next) => {
     postModel.allPost()
+        .then(response => res.status(200).json(response))
+        .catch(error => res.status(500).json({ error }))
 };
 
 exports.likePost = (req, res, next) => {
--- Backend/models/post.js
+++ Backend/models/post.js
@@ -1,16 +1,100 @@
-const postModel = require("../models/post");
 const connectionDB = require("../connexion_mysql");
 
+exports.postSchema = (titre, contenu_text, contenu_media, user_post) => {
+
+    if ((contenu_text && contenu_media) !== null) {
+        connectionDB.query("INSERT INTO Post (titre, contenu_text, contenu_media, user_post) VALUES (?, ?, ?, ?);", [titre, contenu_text, contenu_media, user_post], (error, results) => {
+            if (error) throw error;
+            results;
+            console.log("boucle 1")
+        });
+    }
+    else if (contenu_media !== null) {
+        connectionDB.query("INSERT INTO Post (titre, contenu_media, user_post) VALUES (?, ?, ?);", [titre, contenu_media, user_post], (error, results) => {
+            if (error) throw error;
+            results;
+            console.log("boucle 2")
+        });
+    }
+    else {
+        connectionDB.query("INSERT INTO Post (titre, contenu_text, user_post) VALUES (?, ?, ?);", [titre, contenu_text, user_post], (error, results) => {
+            if (error) throw error;
+            results;
+            console.log("boucle 3")  
+        });
+    } 
+
+    /*try {
+        return new Promise((resolve) => {
+            if ((contenu_text && contenu_media) !== null) {
+                connectionDB.query("INSERT INTO Post (titre, contenu_text, contenu_media, user_post) VALUES (?, ?, ?, ?);", [titre, contenu_text, contenu_media, user_post], (error, results) => {
+                    if (results === undefined) {
+                        resolve (error = "Erreur dans la requête");
+                    }
+                    else {
+                        resolve (results);
+                    }
+                });
+            }
+            else if (contenu_media !== null) {
+                connectionDB.query("INSERT INTO Post (titre, contenu_media, user_post) VALUES (?, ?, ?);", [titre, contenu_media, user_post], (error, results) => {
+                    if (results === undefined) {
+                        resolve (error = "Erreur dans la requête");
+                    }
+                    else {
+                        resolve (results);
+                    }
+                });
+            }
+            else {
+                connectionDB.query("INSERT INTO Post (titre, contenu_text, user_post) VALUES (?, ?, ?);", [titre, contenu_text, user_post], (error, results) => {
+                    if (results === undefined) {
+                        resolve (error = "Erreur dans la requête");
+                    }
+                    else {
+                        resolve (results);
+                    }
+                });
+            } 
+        });
+    }
+    catch (error) {
+        console.log(error);
+    };*/
+}
+
 exports.onePost = (id) => {
-    connectionDB.query("SELECT * FROM Post WHERE id = ?;", [id], function(error, results) {
-        if (error) throw error;
-        results = JSON.parse(JSON.stringify(results));
-    })
+    try {
+        return new Promise((resolve) => {
+            connectionDB.query("SELECT * FROM Post WHERE id = ?;", [id], (error, results) => {
+                if (results === undefined) {
+                    resolve (error = "Erreur dans la requête");
+                }
+                else {
+                    resolve (results);
+                }
+            });
+        });
+    }
+    catch (error) {
+        console.log(error);
+    };
 };
 
 exports.allPost = () => {
-    connectionDB.query("SELECT * FROM Post;", function(error, results) {
-        if (error) throw error;
-        results = JSON.parse(JSON.stringify(results));
-    })
+    try {
+        return new Promise((resolve) => {
+            connectionDB.query("SELECT * FROM Post;", (error, results) => {
+                if (results === undefined) {
+                    resolve (error = "Erreur dans la requête");
+                }
+                else {
+                    resolve (results);
+                }
+            });
+        });
+    }
+    catch (error) {
+        console.log(error);
+    };
 };
