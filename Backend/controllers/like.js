bc34d6764a31d5ad6b54f48aa7a341ef9fc8fd69
McFly91 <martin91200@gmail.com>
2020-12-09 18:57:44 +0100

    Simplification du likePost API

--- Backend/controllers/like.js
+++ Backend/controllers/like.js
@@ -1,20 +1,19 @@
 const postModel = require("../models/post");
 const likeModel = require("../models/like");
 const fs = require("fs");
-const { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } = require("constants");
 
 exports.likePost = (req, res, next) => {
     const userId = res.locals.userId;
     postModel.onePost(req.params.id)
         .then(response => {
-            if (req.body.like === undefined) {
+            if ((req.body.like !== 1) && (req.body.like !== -1) && (req.body.like !== 0)) {
                 return res.status(400).json({ message : "Le corps de la requête n'est pas conforme" })
             }
             else {
                 likeModel.like(userId, response[0].id)
                     .then(likes => {
                         if (req.body.like === 1) {
-                            if (likes[0].like_dislike === 0 ||  likes[0].like_dislike === 1) {
+                            if (likes[0].like_dislike === 0 || likes[0].like_dislike === 1) {
                                 likeModel.modifyLike(1, response[0].id)
                                     .then(() => res.status(200).json({ message : "Like ajouté"}))
                                     .catch(error => res.status(500).json({ error }))
@@ -26,8 +25,10 @@ exports.likePost = (req, res, next) => {
                             }
                         }
                         else if (req.body.like === -1) {
-                            if (likes[0].like_dislike === (-1)) {
-                                res.status(400).json({ message : "Post déjà Disliké" })
+                            if (likes[0].like_dislike === 0 || likes[0].like_dislike === -1) {
+                                likeModel.modifyLike(-1, response[0].id)
+                                    .then(() => res.status(200).json({ message : "Dislike ajouté"}))
+                                    .catch(error => res.status(500).json({ error }))
                             }
                             else if (likes[0].like_dislike === 1) {
                                 likeModel.modifyLike(-1, response[0].id)
